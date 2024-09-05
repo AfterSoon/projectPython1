1efe98b5c63ec7c6e7137c14ad05f556fef4fb98
Putin.V.V <sokorev.sod@yandex.ru>
2024-09-05 20:12:16 +0800

    homework 10_1_1 done

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict, List
+from datetime import datetime
 
 
 def filter_by_state(data: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
@@ -18,22 +19,23 @@ data = [
 filtered_data = filter_by_state(data)
 print(filtered_data)"""
 
-from datetime import datetime
-
 
 def sort_by_date(data: List[Dict[str, Any]], descending: bool = True) -> List[Dict[str, Any]]:
-    """
-    Функция сортирует список словарей по значению ключа 'date'.
-    """
-    return sorted(data, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%d'), reverse=descending)
+    def parse_date(item):
+        return datetime.fromisoformat(item['date'])
+
+    # Сортировка списка по дате
+    return sorted(data, key=parse_date, reverse=descending)
 
 
-"""# Пример использования:
+""""# Пример входных данных
 data = [
-    {'id': 1, 'date': '2023-10-01', 'amount': 100},
-    {'id': 2, 'date': '2023-09-15', 'amount': 200},
-    {'id': 3, 'date': '2023-10-05', 'amount': 300}
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
 ]
 
+# Пример использования функции
 sorted_data = sort_by_date(data)
 print(sorted_data)"""
