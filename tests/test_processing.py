e0a6a91e42bb038831cdc4621653291bf326bae2
Putin.V.V <sokorev.sod@yandex.ru>
2024-09-12 10:37:36 +0800

    homework 10_2_done

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,11 +1,13 @@
-#######
 import pytest
+from typing import List, Dict, Any
 
 from src.processing import filter_by_state, sort_by_date
 
+# Предполагаем, что каждый элемент коллекции имеет такую структуру.
+Transaction = Dict[str, Any]
 
 @pytest.fixture
-def coll():
+def coll() -> List[Transaction]:
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -13,8 +15,7 @@ def coll():
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
 
-
-def test_filter_by_state(coll):
+def test_filter_by_state(coll: List[Transaction]) -> None:
     assert filter_by_state(coll, state="CANCELED") == [
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
@@ -25,8 +26,7 @@ def test_filter_by_state(coll):
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
 
-
-def test_sort_by_date(coll):
+def test_sort_by_date(coll: List[Transaction]) -> None:
     assert sort_by_date(coll) == [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
